4f9c04a96aaf2a5d8e1ddfaafce61ba800c8fdae
avimi13 <alfredorequenez57libra@gmail.com>
2022-11-01 16:19:23 -0600

    Add components and react navigation in my new app

---
+++ Navigation.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+//://www.youtube.com/watch?v=WMOPvJot0yI
+
+//screens
+
+import Home from "./src/screen/Profile";
+import FavoriteScreen from "./src/screen/Home";
+import ScreenAdd from "./src/screen/ScreenAdd";
+import NotificationScreen from "./src/screen/Favorite";
+import ProfileScreen from "./src/screen/ScreenNotification"; 
+
+//Use Icon
+import { Feather } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+
+const HomeStackNaviagator = createNativeStackNavigator();
+
+function Tacks() {
+  return (
+    <HomeStackNaviagator.Navigator initialRouteName="Home">
+      <HomeStackNaviagator.Screen name="Home" component={Home} />
+    </HomeStackNaviagator.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: "red",
+      }}
+    >
+      <Tab.Screen
+        name="Busca tu comida favorite"
+        component={Home}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" size={24} color={color} />
+          ),
+          headerShown: true,
+        }}
+      />
+      <Tab.Screen
+        name="Favorites food"
+        component={FavoriteScreen}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color, size }) => (
+            <Fontisto name="favorite" size={24} color={color} />
+          ),
+          headerShown: true,
+        }}
+      />
+      <Tab.Screen
+        name="Add"
+        component={ScreenAdd}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle" size={35} color="gray" />
+          ),
+          headerShown: true,
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="notifications" size={24} color={color} />
+          ),
+          headerShown: true,
+        }}
+      />
+      <Tab.Screen 
+        name="Mi perfil" 
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user" size={24} color={color} />
+          ),
+          headerShown: true,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
---
+++ src/screen/ScreenAdd.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ScreenAdd = () => {
+    return (
+        <></>
+    )
+}
+export default ScreenAdd;
