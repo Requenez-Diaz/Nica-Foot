cac2f295df3812691d70802754b16fd5d316d8df
alfredorequenez57libra@gmail.com <elliamsanchez28@gmail.com>
2022-11-17 01:06:57 -0600

    cambios en componentes

--- components/home/Search.tsx
+++ components/home/Search.tsx
@@ -1,12 +1,27 @@
 import react from "react"
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const Search = () => {
-    return ( 
-        <View>
-            <Text>Hola buenos dias</Text>
-        </View>
+    return (
+        <TextInput style={styles.container}>
+            <View>
+                <Ionicons name="search-outline" size={30} color="#ffff" />
+                <Text>Search Here</Text>
+            </View>
+        </TextInput>
     )
 
 }
 export default Search;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'gray',
+        padding: 12,
+        marginTop:10,
+        margin: 10,
+        borderRadius: 10
+    }
+})
